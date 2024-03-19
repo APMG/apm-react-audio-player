a04f3f903b707d30fa5727c8cdc0b84e7927ec5c
Jason Phan <jphan@mpr.org>
2024-03-19 14:01:31 -0500

    update styles for mpr news

--- src/components/ReactAudioPlayer/ReactAudioPlayer.js
+++ src/components/ReactAudioPlayer/ReactAudioPlayer.js
@@ -35,100 +35,94 @@ const ReactAudioPlayer = (props) => {
 
   return (
     audioSrc && (
-      <div>
-        <div
-          className='audioPlayer'
-          style={customStyles && customStyles.audioPlayer}
-        >
-          <audio
-            ref={audioPlayerRef}
-            src={audioSrc}
-            preload='metadata'
-            onLoadedMetadata={onLoadedMetadata}
-          />
-          <div className='player-layout'>
-            {volumeCtrl && (
-              <div className='player-controls-secondary-outer'>
-                <div>
-                  <span className='player-volume-text'>
-                    <img src='/img/icon-volume-low.svg' alt='' />
-                    Volume
-                  </span>
-                  <div className='player-controls player-controls-secondary'>
-                    <div className='player-volume'>
-                      <input
-                        type='range'
-                        className='player-timeline-progress js-player-volume-current'
-                        min={0}
-                        max={100}
-                        aria-hidden='true'
-                        onChange={(e) => volumeControl(e)}
-                      />
-                    </div>
+      <div
+        className='audioPlayer'
+        style={customStyles && customStyles.audioPlayer}
+      >
+        <audio
+          ref={audioPlayerRef}
+          src={audioSrc}
+          preload='metadata'
+          onLoadedMetadata={onLoadedMetadata}
+        />
+        <div className='player-layout'>
+          {volumeCtrl && (
+            <div className='player-controls-secondary-outer'>
+              <div>
+                <span className='player-volume-text'>
+                  <img src='/img/icon-volume-low.svg' alt='' />
+                  Volume
+                </span>
+                <div className='player-controls player-controls-secondary'>
+                  <div className='player-volume'>
+                    <input
+                      id='player-volume'
+                      type='range'
+                      className='player-timeline-progress js-player-volume-current'
+                      min={0}
+                      max={100}
+                      aria-hidden='true'
+                      onChange={(e) => volumeControl(e)}
+                    />
                   </div>
                 </div>
               </div>
-            )}
-
-            <div className='player-controls'>
-              <div className='player-btn-play-pause-outer'>
-                <button
-                  onClick={togglePlaying}
-                  className={playBtnClass}
-                  style={customStyles && customStyles.playPause}
-                >
-                  {isPlaying ? <Pause /> : <Play />}
-                </button>
+            </div>
+          )}
+          <div className='player-controls'>
+            <div className='player-btn-play-pause-outer'>
+              <button
+                onClick={togglePlaying}
+                className={playBtnClass}
+                style={customStyles && customStyles.playPause}
+              >
+                {isPlaying ? <Pause /> : <Play />}
+              </button>
+            </div>
+          </div>
+          {customHtml && customHtml}
+          {customProps.audioThumbnailSrc && (
+            <div className='player-thumb-and-text'>
+              <div className='player-thumbnail'>
+                <img
+                  className='player-thumbnail'
+                  src={customProps.audioThumbnailSrc}
+                  alt={customProps.audioThumbnailAlt}
+                />
               </div>
             </div>
-            {customHtml && customHtml}
-            {customProps.audioThumbnailSrc && (
-              <div className='player-thumb-and-text'>
-                <div className='player-thumbnail'>
-                  <img
-                    className='player-thumbnail'
-                    src={customProps.audioThumbnailSrc}
-                    alt={customProps.audioThumbnailAlt}
-                  />
-                </div>
+          )}
+          {!isLive && (
+            <>
+              <div className='player-currentTime'>
+                {calculateTime(currentTime)}
               </div>
-            )}
-            {!isLive && (
-              <>
-                {/* current time */}
-                <div className='player-currentTime'>
-                  {calculateTime(currentTime)}
-                </div>
-                {/* progress bar */}
-                <div className='player-timeline'>
-                  <input
-                    type='range'
-                    className='player-timeline-progress'
-                    defaultValue='0'
-                    aria-hidden='true'
-                    ref={progressBarRef}
-                    onChange={changePlayerCurrentTime}
-                  />
-                </div>
-                {/* duration */}
-                <div
-                  className='player-duration'
-                  style={customStyles && customStyles.duration}
-                >
-                  {duration && !isNaN(duration) && calculateTime(duration)}
-                </div>
-              </>
-            )}
-            {/* title and description */}
-            <div className='player-content'>
-              <div className='player-audio-type type-sm'>
-                {isLive ? (
-                  <div className='player-live-label'>On Air</div>
-                ) : (
-                  <div className='player-title'> {title || ''} </div>
-                )}
-                <div className='player-description'>{description || ''} </div>
+              <div className='player-timeline'>
+                <input
+                  type='range'
+                  className='player-timeline-progress'
+                  defaultValue='0'
+                  aria-hidden='true'
+                  ref={progressBarRef}
+                  onChange={changePlayerCurrentTime}
+                />
               </div>
+              <div
+                className='player-duration'
+                style={customStyles && customStyles.duration}
+              >
+                {duration && !isNaN(duration) && calculateTime(duration)}
+              </div>
+            </>
+          )}
+          <div className='player-content'>
+            <div className='player-audio-type type-sm'>
+              {isLive ? (
+                <div className='player-live-label'>On Air</div>
+              ) : (
+                <div className='player-title'> {title || ''} </div>
+              )}
+              <div className='player-description'>{description || ''} </div>
             </div>
           </div>
         </div>
