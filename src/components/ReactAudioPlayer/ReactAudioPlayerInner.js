f8a1c35614fa649acbb61810c34ee22d8f193360
Geoffrey Hankerson <ghankerson@hankersonosx.mpr.org>
2024-07-31 14:03:43 -0500

    make preload none

--- src/components/ReactAudioPlayer/ReactAudioPlayerInner.js
+++ src/components/ReactAudioPlayer/ReactAudioPlayerInner.js
@@ -47,7 +47,7 @@ const ReactAudioPlayerInner = (props) => {
         <audio
           ref={audioPlayerRef}
           src={audioSrc}
-          preload='metadata'
+          preload='none'
           onLoadedMetadata={onLoadedMetadata}
           muted={isMuted}
         />
